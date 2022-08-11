ce7e1b4e0ea6a1750f6d14f1142b7b9a16cff67f
Andrew <andrew@Andrews-MacBook-Pro.local>
2022-08-11 19:14:06 +0300

    support for ios

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    project: {
-        ios:{},
-        android:{}
-    },
-    assets:['./src/assets/fonts/'],
+	project: {
+			ios:{},
+			android:{}
+	},
+	assets:['./src/assets/fonts/'],
+	dependencies: {
+		'react-native-vector-icons': {
+			platforms: {
+				ios: null,
+			},
+		},
+	},    
 }
